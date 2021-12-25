c6b50ecb9cd1b7f7e437805ecae0637eb0027aea
an-Iceberg <a.g.marosi@bluewin.ch>
2021-12-25 20:23:53 +0100

    minor changes

--- root.sh
+++ root.sh
@@ -2,3 +2,6 @@
 
 apk add curl neovim bspwm sxhkd setxkbmap dbus dbus-x11 rofi picom cmatrix htop lsd btop fish lsblk neofetch feh polybar ranger xrandr xterm font-victor-mono-nerd
 setup-xorg-base
+
+rc-service dbus start
+rc-update add dbus

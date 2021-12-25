959c6bd540a8166bfd06b6ef18e40a7be318f864
an-Iceberg <a.g.marosi@bluewin.ch>
2021-12-25 17:31:23 +0100

    added most of the config files

---
+++ root.sh
@@ -0,0 +1,4 @@
+apk add curl git vim neovim bspwm sxhkd setxkbmap dbus dbus-x11 rofi picom cmatrix htop kitty fish neofetch feh polybar ranger xrandr xterm font-victor-mono-nerd
+
+#cp victormono/* -R /usr/share/fonts/truetype/
+#cp victormononf/* -R /usr/share/fonts/truetype/

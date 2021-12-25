32c45e036fb4a58aa408a07e5d573e6bce08c7ef
an-Iceberg <a.g.marosi@bluewin.ch>
2021-12-25 19:24:37 +0100

    -

--- root.sh
+++ root.sh
@@ -1,1 +1,2 @@
-apk add curl git vim neovim bspwm sxhkd setxkbmap dbus dbus-x11 rofi picom cmatrix htop lsd btop fish neofetch feh polybar ranger xrandr xterm font-victor-mono-nerd
+apk add curl neovim bspwm sxhkd setxkbmap dbus dbus-x11 rofi picom cmatrix htop lsd btop fish lsblk neofetch feh polybar ranger xrandr xterm font-victor-mono-nerd
+setup-xorg-base

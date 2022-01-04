ed5535f2ebd3763acb385170132b64dac2364b25
an-Iceberg <a.g.marosi@bluewin.ch>
2022-01-04 16:24:49 +0100

    added ncdu to install script

--- root.sh
+++ root.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 apk add curl neovim bspwm sxhkd setxkbmap dbus dbus-x11 rofi picom cmatrix htop lsd btop fish lsblk neofetch feh polybar xrandr xterm font-victor-mono-nerd
-setup-xorg-base py3-pip fzf ripgrep starship
+setup-xorg-base py3-pip fzf ripgrep starship ncdu
 
 rc-service dbus start
 rc-update add dbus

909744d055cbcb043d1bb1eab4edba1ff26f2952
an-Iceberg <a.g.marosi@bluewin.ch>
2022-01-09 12:06:07 +0100

    added fzf aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,8 @@ if status is-interactive
 end
 
 starship init fish | source
+
+alias fcd="cd (find -type d | fzf)"
+alias fnvim="nvim (find -type f | fzf)"
+alias frm="rm -r (find . | fzf -m)"
+alias fshred="shred -zfun3 (find . | fzf -m)"

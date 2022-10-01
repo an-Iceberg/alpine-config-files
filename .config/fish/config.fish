64c44dd9c7030d2cc4c38eec733a151ea68121ef
Kazul <82974363+an-Iceberg@users.noreply.github.com>
2022-10-01 18:49:48 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,3 @@ function fish_greeting
 end
 
 starship init fish | source
-
-alias fcd="cd (find / -type d | fzf)"
-alias fnvim="nvim (find / -type f | fzf)"
-alias frm="rm -r (find / . | fzf -m)"
-alias fshred="shred -zfun3 (find / . | fzf -m)"
-alias fcat="cat (find / -type f | fzf)"
-alias fless="less (find / -type f | fzf)"

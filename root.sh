c40b1f7067f7439f241fba2441e29943fd8d922e
Kazul <82974363+an-Iceberg@users.noreply.github.com>
2022-10-01 18:41:34 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
+
 apk update
 apk upgrade
 
 setup-xorg-base
 
-apk add curl bat neovim bspwm sxhkd setxkbmap dbus dbus-x11 rofi picom cmatrix htop lsd btop fish lsblk neofetch feh polybar xrandr xterm font-victor-mono-nerd setup-xorg-base fzf ripgrep starship ncdu nodejs npm cargo
+apk add curl neovim bspwm sxhkd setxkbmap dbus dbus-x11 picom cmatrix htop btop fish lsblk neofetch feh polybar xrandr xterm font-victor-mono-nerd setup-xorg-base #starship
 
 rc-service dbus start
 rc-update add dbus

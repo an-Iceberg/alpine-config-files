a2f1ae9acde5b0758662504f04ec529bfce625e0
Kazul <82974363+an-Iceberg@users.noreply.github.com>
2022-10-01 18:45:40 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -7,5 +7,9 @@ setup-xorg-base
 
 apk add doas curl neovim bspwm sxhkd setxkbmap dbus dbus-x11 picom cmatrix htop btop fish lsblk neofetch feh polybar xrandr xterm font-victor-mono-nerd setup-xorg-base #starship
 
+adduser kazul video
+adduser kazul input
+adduser kazul wheel
+
 rc-service dbus start
 rc-update add dbus
